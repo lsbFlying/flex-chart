f53a6c46bbde9f458f84788d7c29f177c3245ee6
10019630 <liushanbao@shein.com>
2022-09-27 17:48:52 +0800

    feat: update code(upgrade code)

--- src/flex-chart/model/index.ts
+++ src/flex-chart/model/index.ts
@@ -96,7 +96,7 @@ export type EventParams = {
   // 系列名称。当 componentType 为 'series' 时有意义。
   seriesName: string;
   // 数据名，类目名
-  name: string;
+  name: string | number;
   // 数据在传入的 data 数组中的 index
   dataIndex: number;
   // 传入的原始数据项
@@ -106,7 +106,7 @@ export type EventParams = {
   // 其他大部分图表中只有一种 data，dataType 无意义。
   dataType: string;
   // 传入的数据值
-  value: number | (number | string)[];
+  value: number | string | (number | string)[];
   // 数据图形的颜色。当 componentType 为 'series' 时有意义。
   color: string;
   // 用户自定义的数据。只在 graphic component 和自定义系列（custom series）
